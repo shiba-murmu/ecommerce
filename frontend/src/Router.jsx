d0534c4daebeb87249392060bff9cd15c9dd67f5
Shiba Murmu <shibamurmu001@gmail.com>
2025-09-14 19:10:56 +0530

    first commit

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ResponsiveAppBar from './components/Header/ResponsiveAppBar'
+function Router() {
+    return (
+        // <div>r
+        // </div>
+        <>
+            <header>
+                <ResponsiveAppBar />
+            </header>
+            <main>
+                askdjf
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default Router
